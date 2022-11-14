2c7b6eab37a93f77f23c3a2db61ea387530b10c0
Drrpx <117988932+Drrpx@users.noreply.github.com>
2022-11-14 09:08:46 +0100

    Update Main.lua
    added Blox fruit

--- Main.lua
+++ Main.lua
@@ -3,3 +3,9 @@ local Window = Library.CreateLib("Abu Hisham", "BloodTheme")
 
 --Blox Fruit
 local Tab = Window:NewTab("Blox Fruit")
+local BloxSection = Tab:NewSection("Blox Fruit")
+
+
+BloxSection:NewButton("NEVAHUB GUI", "Added On 11/14/2022", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2'))()
+end)
